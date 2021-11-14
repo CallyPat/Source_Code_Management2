85d93dd45ada79085200ee73a4e4235741e64631
CallyPat <patrick.callahan@sinclair.edu>
2021-11-14 17:30:34 -0500

    //changed parameter type from int to float // restructured variable (even though not necessary) // // corrected input variable form n2  to n3

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+//changed parameter type from int to float 
+float average(float i1, float i2, float i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	// restructured variable even though not necessary
+	float x = (i1 + i2 + i3) / 3.0;
+	return x;
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	float n1, n2, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	// corrected input variable form n2  to n3
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(3) << fixed << "The average is " << a << endl;
 
 	return 0;
 }
